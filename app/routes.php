a7e4590f9fcf94df5794bf05207491d3f4d5e07d
b3n10 <francisben.lleve@gmail.com>
2018-05-31 00:44:07 +0800

    Fix comments

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,7 @@
 <?php
 
-// Run index() of HomeController in /home
+// 'HomeController' here is a property of $container obj
+// so it will run the callback function for $container['HomeController']
+// which returns a HomeController obj
+// then run the index method to the requested URI ('/home')
 $app->get('/home', 'HomeController:index');
