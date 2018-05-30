35ca7acfa676f1e44d3323aa40f4c8ea4c77a496
b3n10 <francisben.lleve@gmail.com>
2018-05-30 15:46:50 +0800

    Run index

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,4 @@
 <?php
 
-$app->get('/home', function($request, $response) {
-	// render in 'home.twig' file
-	// $this is the $container
-	return $this->view->render($response, 'home.twig');
-});
+// Run index() of HomeController in /home
+$app->get('/home', 'HomeController:index');
