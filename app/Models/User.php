5132425968b0798cdfd96356fc5100e1ff885e1d
b3n10 <francisben.lleve@gmail.com>
2018-05-30 15:04:34 +0800

    Define class and add comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,5 +1,12 @@
 <?php
 
-// App is name of application defined in composer.json
-// 'app' is it's dir
+// defined in composer.json:
+// App:		name of application
+// 'app':	dir location of App
+//
+// Models is the dir location of User class
 namespace App\Models;
+
+class User {
+
+}
