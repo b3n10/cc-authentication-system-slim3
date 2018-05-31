f816ebb67353d488d7d65e4da538b17057c676c4
b3n10 <francisben.lleve@gmail.com>
2018-06-01 05:27:50 +0800

    Add protected properties to use

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,21 @@
 <?php
 
-// defined in composer.json:
-// App:		name of application
-// 'app':	dir location of App
-//
-// Models is the dir location of User class
 namespace App\Models;
 
-class User {
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model {
+
+	// specify which table to use
+	// by default Eloquent will find and use a table based on the name of class in plural form
+	// class 'User', table will be 'Users' or 'users'
+	protected $table = 'users';
+
+	// specify which cols to use
+	protected $fillable = [
+		'name',
+		'email',
+		'password'
+	];
 
 }
