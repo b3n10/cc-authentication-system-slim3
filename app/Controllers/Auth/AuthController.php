31bcfd2e25f3eddd66c745a12464f48706403781
b3n10 <francisben.lleve@gmail.com>
2018-06-01 05:49:31 +0800

    Add getSignUp method

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -6,6 +6,11 @@ use  App\Controllers\Controller;
 
 class AuthController extends Controller {
 
+	public function getSignUp($request, $response) {
+
+		$this->view->render($response, 'signup.twig');
+
+	}
 
 }
 
