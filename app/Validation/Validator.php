7a397ef85e2c616a4f5cf39a2dc6d79068865744
b3n10 <francisben.lleve@gmail.com>
2018-06-02 01:12:03 +0800

    Try-catch for errors in $rule

--- app/Validation/Validator.php
+++ app/Validation/Validator.php
@@ -14,6 +14,25 @@ class Validator {
 		// loop the $rules array
 		// which has the $field_name and $rule
 		foreach ($rules as $field_name => $rule) {
+
+			try {
+
+				// setName is just the label appended for the $rule
+				// assert will check if the $_POST['field_name'] (sent through $request) passes the $rule
+				$rule->setName(ucfirst($field_name))->assert($request->getParam($field_name));
+
+			} catch (NestedValidationException $e) {
+
+				// Add the exception message of the $rule to $errors array
+				$this->errors[$field_name] = $e->getMessages();
+
+			}
+
+		}
+
+		// return the Validator obj
+		return $this;
+
 	}
 
 }
