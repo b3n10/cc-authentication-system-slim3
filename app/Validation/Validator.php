13974cb50db072c004f74bcde3679939df0ecb9a
b3n10 <francisben.lleve@gmail.com>
2018-06-02 01:11:38 +0800

    Loop thru $rules array

--- app/Validation/Validator.php
+++ app/Validation/Validator.php
@@ -2,10 +2,18 @@
 
 namespace App\Validation;
 
+// include exception class
+use Respect\Validation\Exceptions\NestedValidationException;
+
 class Validator {
+
+	protected $errors;
 
 	public function validate($request, array $rules) {
 
+		// loop the $rules array
+		// which has the $field_name and $rule
+		foreach ($rules as $field_name => $rule) {
 	}
 
 }
