8e407405159be8175a21636ba34eb5461575bf7b
b3n10 <francisben.lleve@gmail.com>
2018-06-02 14:50:25 +0800

    Add comment

--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Middleware;
 
 class ValidationErrorsMiddleware extends Middleware {
 
+	// magic method __invoke is called when ValidationErrorsMiddleware is instantiated
 	public function __invoke($request ,$response, $next_callable_middleware) {
 
 		// invoke $next_callable_middleware passing $request and $response
