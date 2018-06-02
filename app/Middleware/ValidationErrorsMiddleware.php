2c837ab6d02617c42152b3797d669cd02d000ba3
b3n10 <francisben.lleve@gmail.com>
2018-06-02 15:04:29 +0800

    Invoke addGlobal, unset session

--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -7,6 +7,12 @@ class ValidationErrorsMiddleware extends Middleware {
 	// magic method __invoke is called when ValidationErrorsMiddleware is instantiated
 	public function __invoke($request ,$response, $next_callable_middleware) {
 
+		// attach $_SESSION['errors'] to global as errors passed to views (*.twig)
+		$this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+
+		// session not needed so remove it
+		unset($_SESSION['errors']);
+
 		// invoke $next_callable_middleware passing $request and $response
 		$response = $next_callable_middleware($request, $response);
 
