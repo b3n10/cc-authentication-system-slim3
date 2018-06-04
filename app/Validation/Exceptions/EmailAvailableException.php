fd0c437592c87f630db9116e061a4baf41477c25
b3n10 <francisben.lleve@gmail.com>
2018-06-04 14:52:07 +0800

    Add default template for error msg

--- app/Validation/Exceptions/EmailAvailableException.php
+++ app/Validation/Exceptions/EmailAvailableException.php
@@ -7,4 +7,11 @@ use Respect\Validation\Exceptions\ValidationException;
 
 class EmailAvailableException extends ValidationException {
 
+	// template for default error message
+	public static $defaultTemplates = [
+		self::MODE_DEFAULT => [
+			self::STANDARD => 'Email is already taken.'
+		]
+	];
+
 }
