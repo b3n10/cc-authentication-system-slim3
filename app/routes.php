9222b9f1176b5c89061d49f509b9d00bb31c9aaf
b3n10 <francisben.lleve@gmail.com>
2018-06-06 09:47:46 +0800

    Add signin routes

--- app/routes.php
+++ app/routes.php
@@ -6,9 +6,14 @@
 // then run the index method to the requested URI ('/home')
 $app->get('/home', 'HomeController:index')->setName('home');
 
+/* routes for signup page */
 // only setName once for the URI
 // if URI is accessed
 $app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
 
 // if form is submitted
 $app->post('/auth/signup', 'AuthController:postSignUp');
+
+/* routes for signin page */
+$app->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
+$app->post('/auth/signin', 'AuthController:postSignIn');
