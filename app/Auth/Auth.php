b6b03e0bffe749b4130f3e93c4b0c71af050bcde
b3n10 <francisben.lleve@gmail.com>
2018-06-06 10:15:20 +0800

    Update attempt method

--- app/Auth/Auth.php
+++ app/Auth/Auth.php
@@ -2,10 +2,31 @@
 
 namespace App\Auth;
 
+// include User class which has Eloquent for db access
+use App\Models\User as u;
+
 class Auth {
 
 	public function attempt($email, $password) {
 
+		// grab user email
+		$user = u::find('email', $email)->first();
+
+		// if no result for $user
+		if (!$user) {
+			return false;
+		}
+
+		if (password_verify($password, $user->password)) {
+
+			// add userid to Sessions
+			$_SESSION['userid'] = $user->id;
+			return true;
+
+		}
+
+		// if password doesn't match
+		return false;
 
 	}
 
